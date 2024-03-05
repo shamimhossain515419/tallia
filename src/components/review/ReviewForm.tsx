e89c79ad2a482c7881e1bfb87f4e3035e1165522
Shamimusman515419 <shamimhossain01617@gmail.com>
2024-03-05 17:13:07 +0600

    review form

--- src/components/review/ReviewForm.tsx
+++ src/components/review/ReviewForm.tsx
@@ -1,10 +1,9 @@
 "use client";
-
 import { useState } from "react";
-import ReactStars from "react-rating-stars-component";
-
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 const ReviewForm = () => {
-    const [rating, setTating] = useState(1);
+    const [ratings, setRating] = useState(1);
     return (
         <>
             <div className=" pt-5">
@@ -15,16 +14,10 @@ const ReviewForm = () => {
                 <form action="" className=" py-5 ">
                     {/* Your rating  */}
                     <p className="text-[14px] font-normal">Your rating</p>
-                    <ReactStars
-                        className={" text-wrap"}
-                        count={5}
-                        onChange={(e: any) => setTating(e)}
-                        size={24}
-                        isHalf={true}
-                        emptyIcon={<i className="far fa-star"></i>}
-                        halfIcon={<i className="fa fa-star-half-alt"></i>}
-                        fullIcon={<i className="fa fa-star"></i>}
-                        activeColor="#ffc53e"
+                    <Rating
+                        style={{ maxWidth: 180 }}
+                        value={ratings}
+                        onChange={setRating}
                     />
                     <div className=" py-2">
                         {/* your review  */}
@@ -35,14 +28,16 @@ const ReviewForm = () => {
                     </div>
                     <div className=" py-2">
                         {/* your name  */}
-                        <input type="text"
+                        <input
+                            type="text"
                             className="w-full  px-4 py-4 border border-[#00000064] outline-0 rounded-md"
                             placeholder="Your name"
                         />
                     </div>
                     <div className=" py-2">
                         {/* your email  */}
-                        <input type="email"
+                        <input
+                            type="email"
                             className="w-full  px-4 py-4 border border-[#00000064] outline-0 rounded-md"
                             placeholder="Your email"
                         />
@@ -50,10 +45,15 @@ const ReviewForm = () => {
                     <div className="flex items-center gap-3">
                         {/* your email  */}
                         <input type="checkbox" name="" id="" />
-                        <p className="text-[14px] font-medium py-2">Save my name, email, and website in this browser for the next time I comment.</p>
+                        <p className="text-[14px] font-medium py-2">
+                            Save my name, email, and website in this browser for the next time
+                            I comment.
+                        </p>
                     </div>
                     <div className="py-2">
-                        <button className={`text-[15px] lg:text-[20px] w-full  px-6 py-[10px]   my-3 flex items-center justify-center rounded-[40px] cursor-pointer relative overflow-hidden transition-all  border  text-white   duration-500 ease-in-out hover:scale-100 hover:shadow-lg   bg-[#F15A24]`}>
+                        <button
+                            className={`text-[15px] lg:text-[20px] w-full  px-6 py-[10px]   my-3 flex items-center justify-center rounded-[40px] cursor-pointer relative overflow-hidden transition-all  border  text-white   duration-500 ease-in-out hover:scale-100 hover:shadow-lg   bg-[#F15A24]`}
+                        >
                             Sumit
                         </button>
                     </div>
